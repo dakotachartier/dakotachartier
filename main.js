b5f8cb5f69d0256b9223ab0bfb4e0d2a6abb38a4
dakotachartier <63308279+dakotachartier@users.noreply.github.com>
2020-04-14 17:54:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("Up and running!");
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = []
+
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+	console.log("User flipped" + cards[cardId])
+	flipCard(0);
+	flipCard(2);
+
+checkForMatch();
+}
+
+  if (cardsInPlay.length === 2) {
+
+  
+}
